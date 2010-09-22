986ec38571f485e86290d93eacf3154623ad7c98
Martin Solli <martin.solli@budstikka.no>
2010-09-22 09:55:23 +0200

    Grunnleggende layout.

--- public/javascripts/typekit.js
+++ public/javascripts/typekit.js
@@ -1,2 +1,1 @@
-<script type="text/javascript" src="http://use.typekit.com/odc3uil.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+try{Typekit.load();}catch(e){}
