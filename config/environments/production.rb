2d4043de95da835d66827aad19b8bd6d1201a725
Martin Solli <martin.solli@budstikka.no>
2010-09-23 10:51:37 +0200

    deprecation = :notify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Mime::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -46,4 +46,6 @@ Mime::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+
+  config.active_support.deprecation = :notify
 end
