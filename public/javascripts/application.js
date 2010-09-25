4aedb2bcf245ea0945bb416b2a4d845292bcceba
Martin Solli <martin.solli@budstikka.no>
2010-09-25 13:40:09 +0200

    Ny fiks for FOUT i Firefox.

---
+++ public/javascripts/application.js
@@ -0,0 +1,11 @@
+(function(){
+  // if firefox 3.5+, hide content till load (or 3 seconds) to prevent FOUT
+  var d = document, e = d.documentElement, s = d.createElement('style');
+  if (e.style.MozTransform === ''){ // gecko 1.9.1 inference
+    s.textContent = 'body{visibility:hidden}';
+    e.firstChild.appendChild(s);
+    function f(){ s.parentNode && s.parentNode.removeChild(s); }
+    addEventListener('load',f,false);
+    setTimeout(f,3000); 
+  }
+})();
