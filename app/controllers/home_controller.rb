141e2eb4c206982c13171c1cfd6fb179c819d650
Martin Solli <msolli@gmail.com>
2010-10-28 13:40:37 +0200

    Sorterer artikler i alfabetisk-visning.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,8 @@ class HomeController < ApplicationController
 
   def alphabetic
     @letter = params[:letter]
-    @articles = Article.where(:headword => /^(\p{P})?#{@letter}/i)
+    @articles = Article.where(:headword => /^(\p{P})*#{@letter}/i).all.sort do |a, b|
+      a.headword.sub(/^(\p{P})+/, '').mb_chars.downcase <=> b.headword.sub(/^(\p{P})+/, '').mb_chars.downcase
+    end
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 
 require 'spec_helper'
 
@@ -25,11 +25,48 @@ describe HomeController do
       assigns(:articles).first.headword.should == "«Hartmann»"
     end
 
+    it "shows articles where the several first chars of headword are not letters" do
+      Article.create!(:headword => "«'Hartmann'»")
+      get :alphabetic, {:letter => 'h'}
+      response.should be_success
+      assigns(:articles).first.headword.should == "«'Hartmann'»"
+    end
+
     it "handles norwegian characters i url" do
       Article.create!(:headword => "Åh")
       get :alphabetic, {:letter => 'å'}
       response.should be_success
       assigns(:articles).first.headword.should == "Åh"
+    end
+
+    it "sorts articles alphabetically" do
+      Article.create!(:headword => "Ax")
+      Article.create!(:headword => "Ab")
+      get :alphabetic, {:letter => 'a'}
+      assigns(:articles).first.headword.should == "Ab"
+    end
+
+    it "sorts articles with different case" do
+      Article.create!(:headword => "Ax")
+      Article.create!(:headword => "ab")
+      get :alphabetic, {:letter => 'a'}
+      assigns(:articles).first.headword.should == "ab"
+    end
+
+    it "sorts articles where first char is not a letter" do
+      Article.create!(:headword => "«Ab»")
+      Article.create!(:headword => "Ax")
+      get :alphabetic, {:letter => 'a'}
+      assigns(:articles).first.headword.should == "«Ab»"
+      assigns(:articles).last.headword.should == "Ax"
+    end
+
+    it "sorts articles where several first chars are not letters" do
+      Article.create!(:headword => "«'Ab'»")
+      Article.create!(:headword => "Ax")
+      get :alphabetic, {:letter => 'a'}
+      assigns(:articles).first.headword.should == "«'Ab'»"
+      assigns(:articles).last.headword.should == "Ax"
     end
   end
 end
