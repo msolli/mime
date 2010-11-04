5e487396cb7fa6101b31d3dbc3c0033331981411
Peter Haza <peter.haza@gmail.com>
2010-11-04 11:53:56 +0100

    Refactor additional steps out in their own file to make updates to cucumber easier

--- features/step_definitions/web_steps_no_local.rb
+++ features/step_definitions/web_steps_no_local.rb
@@ -9,3 +9,19 @@ end
 Så /^(?:|skal )feltet "([^"]*)" ikke (?:|skal )inneholde "([^"]*)"$/ do |field, value|
   Then %{the "#{field}" field should not contain "#{value}"}
 end
+
+Then /^"([^\"]*)" should be visible$/ do |selector|
+  assert_not_nil page.has_css?(selector, :visible => true)
+end
+
+Then /^"([^\"]*)" should not be visible$/ do |selector|
+  page.has_css?(selector, :visible => true).should be_false
+end
+
+Så /^(?:skal )"([^"]*)" være synlig$/ do |selector|
+  Then %{"#{selector}" should be visible}
+end
+
+Så /^(?:skal )ikke "([^"]*)" være synlig$/ do |selector|
+  Then %{"#{selector}" should not be visible}
+end
