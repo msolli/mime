334882a2f320d7deb897b757df05611947070ec1
Martin Solli <msolli@gmail.com>
2010-11-12 12:59:49 +0100

    Fjernet flash fra cachede sider.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     options[:class] ||= "timeago"
     content_tag(:time, l(time.to_date, :format => :long), options.merge(:datetime => time.getutc.iso8601)) if time
   end
+
+  def cached_page?
+    controller_name == 'home' && ['index', 'alphabetic'].include?(action_name)
+  end
 end
