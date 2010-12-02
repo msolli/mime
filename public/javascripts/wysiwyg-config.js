dd94ebe09aa6c90904497eb1d03b34008f1da8c6
Peter Haza <peter.haza@gmail.com>
2010-12-02 20:55:09 +0100

    Fjern media embed og gjør editoren høyere

--- public/javascripts/wysiwyg-config.js
+++ public/javascripts/wysiwyg-config.js
@@ -4,13 +4,14 @@ CKEDITOR.editorConfig = function( config )
 	config.toolbarCanCollapse = false;
 	config.resize_dir = 'vertical';
 	config.format_tags = 'p;h2;h3';
-	config.extraPlugins = 'MediaEmbed,mimeimage,mimelink,autogrow';
+	config.extraPlugins = 'mimeimage,mimelink,autogrow';
 	config.removePlugins = 'link,forms,image'; // Remove this to avoid the plugin registering for doubleclicks on links
+	config.autoGrow_minHeight = 500;
 	config.defaultLanguage = 'nb';
 	config.contentsCss = ['http://yui.yahooapis.com/3.2.0/build/cssreset/reset-min.css', '/stylesheets/wysiwyg.css'];
 	config.toolbar_mime = [
 		[	'Format',
-			'Bold', 'Italic', 'Underline', 'MimeImage', 'MimeLink', 'Unlink', 'MediaEmbed', '-',
+			'Bold', 'Italic', 'Underline', 'MimeImage', 'MimeLink', 'Unlink', '-',
 			// Leave out image for now until the server side is ready
 			// 'NumberedList', 'BulletedList', 'Image', 'Table', '-', 
 			'NumberedList', 'BulletedList', 'Table', '-',
