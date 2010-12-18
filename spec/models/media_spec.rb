61a1f006d766aa647b31d6b6f90b068f10acf51f
Peter Haza <peter.haza@gmail.com>
2010-12-19 00:50:27 +0100

    Test for async media-håntering i modellen

--- spec/models/media_spec.rb
+++ spec/models/media_spec.rb
@@ -7,5 +7,22 @@ describe Media do
   it { should have_fields(:description, :author, :file_uid).of_type(String) }
   it { should have_field(:date).of_type(DateTime) }
   it { should respond_to(:file) }
-
+  
+  describe "Article with media" do
+    it "should add async media uploads" do
+      article = Article.new :headword => 'Foo'
+    
+      media1 = Media.create :file => open("#{Rails.root}/spec/data/gif.gif")
+      media2 = Media.create :file => open("#{Rails.root}/spec/data/jpeg.jpeg")
+      media3 = Media.create :file => open("#{Rails.root}/spec/data/png.png")
+    
+      article.media_ids_from_async_upload = "#{media1.id} #{media2.id} #{media3.id}"
+    
+      article.save
+    
+      article.medias.length.should == 3
+      article.medias.first.file.mime_type.should == 'image/gif'
+    end
+  end
+  
 end
