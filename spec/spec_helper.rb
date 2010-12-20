49923633b5e572c8935957963b31bd88436192f1
Martin Solli <msolli@gmail.com>
2010-12-20 13:59:12 +0100

    Setter locale for RSpec eksplisitt.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+I18n.locale = :'no-NB'
+
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
