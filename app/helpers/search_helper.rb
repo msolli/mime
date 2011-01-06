696cb83c788bfb2d5053e61ef3d0962a29bf1a58
Peter Haza <peter.haza@gmail.com>
2011-01-06 13:54:35 +0100

    Refaktorér for å vise 0 av 0 og 1 av 1 treff i søk

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,4 +6,14 @@ module SearchHelper
     res.blank? ? nil : res
   end
   
+  def paginate(hits)
+    # craeator of will_paginate, for some reason, has decided that they should not render anything
+    # if there are less than 2 hits in the result set
+    if hits.total_entries > 1
+      will_paginate hits, :renderer => TemplatePaginationRenderer, :template => 'search.template.several_hits'
+    else
+      t('search.template.one_or_less_hits', :count => hits.total_entries)
+    end
+  end
+  
 end
