403d07df9b79a5aa8e9b1c87aa6eccc7629b67fb
Peter Haza <peter.haza@gmail.com>
2011-01-10 15:33:49 +0100

    Bruk stub av autoscale i test inntil videre

--- config/initializers/heroku_autoscale.rb
+++ config/initializers/heroku_autoscale.rb
@@ -1,1 +1,2 @@
 HerokuDelayedJobAutoscale::Autoscale.autoscale_manager = HerokuDelayedJobAutoscale::Manager::Heroku if Rails.env.production?
+HerokuDelayedJobAutoscale::Autoscale.autoscale_manager = HerokuDelayedJobAutoscale::Manager::Stub if Rails.env.test? || Rails.env.cucumber?
