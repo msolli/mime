f6eac83cc95f6b8f25e114c77ba86f2b8f5c6035
Peter Haza <peter.haza@gmail.com>
2011-01-13 10:12:54 +0100

    Generaliser søket mer

--- app/controllers/json_search_controller.rb
+++ app/controllers/json_search_controller.rb
@@ -7,14 +7,22 @@ class JsonSearchController < ActionController::Metal
   # * :model - Article
   # * :query - keyword query
   # * :num   - number of hits
+  # * :qfields - Fields to query
+  # * :rfields - Fields to return, space separated
   def new
     if params[:model] && params[:query]
       _model = params[:model].capitalize.constantize
       if _model.searchable?
         res = _model.search do
-          keywords params[:query] do
-            fields(:headword)
+          if params[:qfields]
+            _fields = params[:qfields].split
+            keywords params[:query] do
+              fields(*_fields)
+            end
+          else
+            keywords params[:query]
           end
+          
           paginate :page => 1, :per_page => params[:num] || 10
         end
         
@@ -30,8 +38,17 @@ class JsonSearchController < ActionController::Metal
   private
   def result_to_render(result)
     results = []
+    rfields = (params[:rfields] || "url headword").split.map(&:to_sym)
     result.results.each do |result|
-      results << {:headword => result.headword, :url => pretty_article_path(result)}
+      obj = {}
+      obj[:url] = pretty_article_path(result) if result.is_a?(Article)
+      
+      rfields.each do |field|
+        return if field == :url
+        obj[field] = result.send(field)
+      end
+      
+      results << obj
     end
     results
   end
