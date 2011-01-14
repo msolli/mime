89533f7adaa62f839c338b74b43f574a503855b4
Peter Haza <peter.haza@gmail.com>
2011-01-14 11:26:54 +0100

    Forenkle renderingen noe

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,2 +1,14 @@
 module VersionsHelper
+  
+  def version_number_or_text(version, count)
+    case
+    when count == 0
+      t('articles.versions.active')
+    when version.version == 1
+      t('articles.versions.first')
+    else
+      t('articles.versions.print', :version => version.version)
+    end
+  end
+  
 end
