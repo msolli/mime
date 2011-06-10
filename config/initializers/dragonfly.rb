c5964e521cbe54d12ef124f7a3af495525f93ca9
Martin Solli <msolli@gmail.com>
2011-06-10 12:28:25 +0200

    S3 datastore i dev, Mongoid i test

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,15 +1,11 @@
 app = Dragonfly[:images]
 
 app.configure_with(:imagemagick)
-if Rails.env.production?
+case Rails.env
+when "production"
   app.configure_with(:rails)
   app.configure_with(:heroku, ENV['S3_BUCKET'])
-else
-  ## Mongoid
-  # app.configure_with(:rails) do |c|
-  #   c.datastore = Dragonfly::DataStorage::MongoDataStore.new :db => Mongoid.database
-  # end
-
+when "development"
   ## S3
   app.datastore = Dragonfly::DataStorage::S3DataStore.new
   app.datastore.configure do |c|
@@ -17,6 +13,11 @@ else
     c.access_key_id = ENV['S3_KEY']
     c.secret_access_key = ENV['S3_SECRET']
     c.region = 'eu-west-1'
+  end
+when "test"
+  ## Mongoid
+  app.configure_with(:rails) do |c|
+    c.datastore = Dragonfly::DataStorage::MongoDataStore.new :db => Mongoid.database
   end
 end
 
