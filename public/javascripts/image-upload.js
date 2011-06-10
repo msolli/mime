cf9ecb4357df5c3942a4a5f65f4f6c41dc164b73
Martin Solli <msolli@gmail.com>
2011-06-10 12:05:49 +0200

    Bildeopplasting funker nå også etter at første bilde er lastet opp.

--- public/javascripts/image-upload.js
+++ public/javascripts/image-upload.js
@@ -1,54 +1,66 @@
-$(function() {
+var ImageUpload = (function() {
+
+  var uploader,
+
+  init = function() {
+    $('input:submit, #upload-image').button();
+    uploader = new plupload.Uploader({
+      runtimes: 'html5,html4',
+      browse_button: 'upload-image',
+      container: 'upload-container',
+      max_file_size: '10mb',
+      multi_selection: false,
+      multipart_params: {
+        authenticity_token: $('meta[name="csrf-token"]').attr('content')
+  		},
+      url: $('#upload-container').data('url'),
+      filters: [
+        {title: 'Bilder', extensions: 'jpg,jpeg,gif,png'}
+      ],
+
+      init: {
+
+        FilesAdded: function(up, files) {
+          $('#thumb-loader').show();
+          $('#upload-image').button('disable');
+          uploader.start();
+        },
+
+        FileUploaded: function(up, file, response) {
+          $('#edit_image').fadeOut('fast', function() {
+            $(this).html(response.response);
+            $(this).fadeIn('fast');
+          });
+        },
+
+        Error: function(up, args) {
+          log(args);
+        }
+      }
+    });
+    uploader.init();
+
+    $('form.image')
+      .live('ajax:beforeSend', function(evt, xhr, settings) {
+        $('#image-loader').show();
+      })
+      .live('ajax:success', function(evt, data, status, xhr) {
+      })
+      .live('ajax:complete', function(evt, xhr, status) {
+        $('#image-loader').hide();
+      });
+  };
+
+  return {
+    init: init
+  };
+}());
+
+
+$(document).ready(function() {
   if ($('#upload-image').length == 0) {
     return;
   }
 
-  $('input:submit, #upload-image').button();
-
-  var uploader = new plupload.Uploader({
-    runtimes: 'html5,html4',
-    browse_button: 'upload-image',
-    container: 'upload-container',
-    max_file_size: '10mb',
-    multi_selection: false,
-    multipart_params: {
-      authenticity_token: $('meta[name="csrf-token"]').attr('content')
-		},
-    url: $('#upload-container').data('url'),
-    filters: [
-      {title: 'Bilder', extensions: 'jpg,jpeg,gif,png'}
-    ],
-
-    init: {
-
-      FilesAdded: function(up, files) {
-        $('#thumb-loader').show();
-        $('#upload-image').button('disable');
-        uploader.start();
-      },
-
-      FileUploaded: function(up, file, response) {
-        $('#edit_image').fadeOut('fast', function() {
-          $(this).html(response.response);
-          $(this).fadeIn('fast');
-        });
-      },
-
-      Error: function(up, args) {
-        log(args);
-      }
-    }
-  });
-
-  uploader.init();
-
-  $('form.image')
-    .live('ajax:beforeSend', function(evt, xhr, settings) {
-      $('#image-loader').show();
-    })
-    .live('ajax:success', function(evt, data, status, xhr) {
-    })
-    .live('ajax:complete', function(evt, xhr, status) {
-      $('#image-loader').hide();
-    });
+  ImageUpload.init();
 });
