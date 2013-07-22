890ffbfddbebd0280f7a19d83554c259baace848
Theodor Tonum <theodor@tonum.no>
2013-07-22 17:38:35 +0200

    Setup javascript combines according to assets.yml

---
+++ app/assets/javascripts/mobile.js
@@ -0,0 +1,9 @@
+//= require webfont-loader
+//= require spring
+//= require jquery
+//= require jquery/jquery.templates/beta1/jquery.tmpl
+//= require jquery/jquery.timeago/0.9.2/jquery.timeago
+//= require jquery/jquery.timeago/common/jquery.timeago.no
+//= require mobile/jquery-mobile-config
+//= require jquery.mobile-1.0a3/jquery.mobile-1.0a3
+//= require mobile/mobile-application
