4d42f08c30db393e17895c4f92a4c8e9ed2c2f3c
Theodor Tonum <theodor@tonum.no>
2013-07-23 16:12:51 +0200

    Revert zeus config

--- custom_plan.rb
+++ custom_plan.rb
@@ -2,15 +2,15 @@ require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
 
-  def default_bundle_with_test_env
-    ::Rails.env = 'test'
-    ENV['RAILS_ENV'] = 'test'
-    default_bundle
-  end
+  # def default_bundle_with_test_env
+  #   ::Rails.env = 'test'
+  #   ENV['RAILS_ENV'] = 'test'
+  #   default_bundle
+  # end
 
-  def test_rake
-    rake
-  end
+  # def test_rake
+  #   rake
+  # end
 
 end
 
