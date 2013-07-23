d4300e33cac47bd65c0079625a1547a7af539e37
Theodor Tonum <theodor@tonum.no>
2013-07-23 09:50:40 +0200

    Add test_rake to zeus

--- custom_plan.rb
+++ custom_plan.rb
@@ -2,9 +2,15 @@ require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
 
-  # def my_custom_command
-  #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
-  # end
+  def default_bundle_with_test_env
+    ::Rails.env = 'test'
+    ENV['RAILS_ENV'] = 'test'
+    default_bundle
+  end
+
+  def test_rake
+    rake
+  end
 
 end
 
